7fe2f1fabbaf6621b22904d157422ff9db1d05af
shileide321 <burningstyle@sina.com>
2013-06-23 17:00:36 +0800

    实现初步内存中的数据库导出和导入外部文件的功能

--- StudentInfo.c
+++ StudentInfo.c
@@ -30,7 +30,7 @@ typedef struct studentLinkList{
 //-----------------------------------------------------------------函数声明
 //创建数据库
 studentLink *newDataBase();
-studentLink *newDataBaseFromFile(FILE *f); //通过导入文件建立数据库
+studentLink *newDataBaseFromFile(FILE **f); //通过导入文件建立数据库
 int newDataBase2(studentLink **head);//这样也可以实现正确的头结点申请
 //选择，添加，删除,清空
 int addRecord(studentLink **head);
@@ -43,7 +43,7 @@ int printList(studentLink **head);
 int inputStudentInfo(studentInfo *elem);
 int printStudentInfo(studentInfo elem);
 //数据输出到文件
-int outputToFile(studentLink **head,FILE *f);
+int outputToFile(studentLink **head,FILE **f);
 
 
 
@@ -52,17 +52,21 @@ int main(){
 
     char ch='^';
     studentLink *dataBase=NULL;//数据链表头结点，可以当做数据库的名称
+    FILE 		*dataBaseFile=NULL;
 
     while(ch!='$'){
         char c=0;
 		printf ( "=============================\n" );
 		if(dataBase==NULL){
 			printf ( "a.新建数据档案\n" );
+			printf ( "z.从数据库文件提取信息\n" );
 		}else{
 			printf ( "b.添加数据\n" );
 			printf ( "c.删除数据\n" );
 			printf ( "d.数据查询\n" );
 			printf ( "e.清除数据库\n" );
+			printf ( "f.遍历数据库\n" );
+			printf ( "o.导出至数据库文件\n" );
 			printf ( "q.退出程序\n" );
 		}
 		printf ( "\n=============================\n" );
@@ -73,6 +77,8 @@ int main(){
 		switch(ch){
 			case 'a':
                 dataBase=newDataBase();break;//显示输入示例
+            case 'z':
+				dataBase=newDataBaseFromFile(&dataBaseFile);break;
 			case 'b':
 				addRecord(&dataBase);break;
 			case 'c':
@@ -83,6 +89,8 @@ int main(){
 				clear(&dataBase);break;
 			case 'f':
 				printList(&dataBase);system("pause");break;
+			case 'o':
+				outputToFile(&dataBase,&dataBaseFile);break;
 			case 'q':
 				clear(&dataBase);exit(0);break;
 			default :
@@ -157,6 +165,7 @@ studentLink *selectRecord(studentLink **head){
 	printf ( "4.birthday\n" );
 	printf ( "5.telephone\n" );        
 	printf ( "\n=============================\n" );
+    printf ( "(example:sno 001)\n" );        
 	scanf("%s",tempSelectName);
 	if(strcmp(tempSelectName,sno_name)==0){
         studentLink *p=NULL;
@@ -266,3 +275,54 @@ int inputStudentInfo(studentInfo *elem){
 	scanf("%s",elem->telephone);
 	printf("\n");
 }
+
+studentLink *newDataBaseFromFile(FILE **f){
+	studentLink *head;
+	studentLink temp;    
+	head=newDataBase();
+	if(*f=fopen("DataBase.dat","rb"))
+    while(!feof(*f))
+	{
+		
+		studentLink *newStudentInfo;
+	    if((head)!=NULL){
+	        newStudentInfo=(studentLink *)malloc(sizeof(studentLink));
+	        if(newStudentInfo!=NULL){
+	            
+		        fscanf(*f,"%s",newStudentInfo->stu.sno);
+		        fscanf(*f,"%s",newStudentInfo->stu.name);
+		        fscanf(*f,"%d",&(newStudentInfo->stu.sex));
+		        fscanf(*f,"%s",newStudentInfo->stu.birthday);
+		        fscanf(*f,"%s",newStudentInfo->stu.telephone);
+
+				newStudentInfo->next=(head)->next;
+		        (head)->next=newStudentInfo; 
+
+	        }
+    	}
+	}
+    return head;
+} 
+
+int outputToFile(studentLink **head,FILE **f){
+	//此函数暂时不可判断数据的重复性 
+    studentLink *p=NULL;
+    //控制文本文件中没有多余的空格符 
+	int i=0;
+    if((p=*head)!=NULL)
+    //全部导入，因为在引入时 就是全部引入 
+	if(*f=fopen("DataBase.dat","w")){
+		while(p->next!=NULL && (i=i+1)){
+			p=p->next;
+			if(i>1)fprintf(*f,"\n");
+			fprintf(*f,"%s ",p->stu.sno);
+			fprintf(*f,"%s ",p->stu.name);
+			fprintf(*f,"%d ",p->stu.sex);
+			fprintf(*f,"%s ",p->stu.birthday);
+			fprintf(*f,"%s",p->stu.telephone);
+			
+		}
+		fclose(*f);
+	}
+}
+
